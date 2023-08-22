d391f20c83e1c540f6a8090c42aa07115ee29837
Sam Coppard <samcoppard@Sams-MacBook-Pro.local>
2023-08-22 17:07:17 +0100

    Small improvements

--- Problem 12/attempt_1.py
+++ Problem 12/attempt_1.py
@@ -4,14 +4,14 @@
 # Combinations of 2 and 3 will give the most divisors for the lowest number, so we only
 # need to check multiples of 6
 
-# Need an efficient way to find factors of a given number x
 import time
-from math import sqrt
-
 start_time = time.time()
 
+from math import sqrt
+
+# Need an efficient way to count how many factors a given number has
 def count_factors(num):
-    factors = {1, num, 2, int(num/2), 3, int(num/3)} # We're only going to check multiples of 6
+    factors = {1, num, 2, int(num/2), 3, int(num/3)} # We're only going to check multiples of 6 here
 
     for i in range(4, int(sqrt(num)) + 1):
         if num % i == 0:
@@ -21,16 +21,15 @@ def count_factors(num):
 
 
 max_factors = 0
-n = 707
+n = 1
 
 while max_factors < 500:
     triangle_num = (n/2) * (n+1)
     if triangle_num % 12 == 0:
         max_factors = max(max_factors, count_factors(triangle_num))
-    if max_factors < 500:
-        n += 1
-    else:
-        print(f"{int(triangle_num)} is the {n}th triangle number, and the first to have over 500 factors - it's got {max_factors} factors")
+    n += 1
+
+print(f"{int(triangle_num)} is the {n}th triangle number, and the first to have over 500 factors - it's got {max_factors} factors")
 
 end_time = time.time()
 
