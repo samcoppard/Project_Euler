6b67e98b2bf4be24670aa82b83c3732bc00bf9e9
Sam Coppard <samcoppard@Sams-MacBook-Pro.local>
2023-08-29 15:49:48 +0100

    Faster solution and timings

--- Problem 15/attempt_1.py
+++ Problem 15/attempt_1.py
@@ -10,8 +10,8 @@ How many such routes are there through a 20x20 grid? """
 # Also good to note that (a, b) == (b, a) here.
 
 # This all means that we can find the number of routes through any given grid by simply
-# adding the number of routes through a set of smaller grids. We just have to make sure
-# we don't do it recursively to avoid computational overload.
+# adding the number of routes through a set of smaller grids. We just have to use
+# memoization to make it speedy
 
 import time
 
@@ -25,25 +25,28 @@ routes = [[0] * 21 for _ in range(21)]
 for i in range(1, 21):
     routes[i][1] = i + 1
 
+
 def calc_routes(m, n) -> int:
     if n > m:
-        return calc_routes(n, m) # Reduce duplication
+        return calc_routes(n, m)  # Reduce duplication
     elif routes[m][n] != 0:
-        return routes[m][n] # Reuse what we've already calculated
+        return routes[m][n]  # Reuse what we've already calculated
     else:
-        number_of_routes = 2 # Start with the 2 trivial routes around the outside of the grid
+        number_of_routes = (
+            2  # Start with the 2 trivial routes around the outside of the grid
+        )
         # Implement the formula commented above
         for i in range(1, m):
             # Add the stored number of routes for each subgrid where available; calculate if not available
-            if routes[i][n-1] != 0:
-                number_of_routes += routes[i][n-1]
+            if routes[i][n - 1] != 0:
+                number_of_routes += routes[i][n - 1]
             else:
-                number_of_routes += calc_routes(i, n-1)
+                number_of_routes += calc_routes(i, n - 1)
         for j in range(1, n):
-            if routes[m-1][j] != 0:
-                number_of_routes += routes[m-1][j]
+            if routes[m - 1][j] != 0:
+                number_of_routes += routes[m - 1][j]
             else:
-                number_of_routes += calc_routes(m-1, j)
+                number_of_routes += calc_routes(m - 1, j)
         # Assign the result to the list of lists of numbers of possible routes
         routes[m][n] = number_of_routes
         return number_of_routes
@@ -56,4 +59,3 @@ end_time = time.time()
 print(f"There are {routes[20][20]} possible routes through a 20x20 grid")
 
 print(f"This took {(end_time - start_time):.6f} seconds to run")
-
